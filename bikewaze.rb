6e19ecd75a78edbeaef8d1bac0d296bc502f5400
Gazal Garg <ggarg@kiwiup.com>
2014-09-20 21:13:11 +0530

    post & get working

--- bikewaze.rb
+++ bikewaze.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'active_record'
 
 class Bikepark < ActiveRecord::Base
-  
+  self.table_name = "bikeparks"
 end
 
 get '/' do
@@ -19,5 +19,43 @@ get '/' do
   a.save
   body 'hello'
 end
+
+get '/checkin' do
+  ActiveRecord::Base.establish_connection(
+                                          :adapter  => "mysql2",
+                                          :host     => "localhost",
+                                          :username => "root",
+                                          :password => "root",
+                                          :database => "bikespot"
+                                          )
+
+  @x = params[:x]
+  @y = params[:y]
+  @available = params[:available]
+  a = Bikepark.new(:x => @x, :y => @y, :capacity => @available)
+  a.save
+  # if x.nil?
+  #   body "no"
+  # else
+  #   body  "moo"
+  # end
+end
+
+get '/availability' do
+  con
+  x = Bikepark.last
+  body x.id.to_s
+end
+
+def con
+  ActiveRecord::Base.establish_connection(
+                                          :adapter  => "mysql2",
+                                          :host     => "localhost",
+                                          :username => "root",
+                                          :password => "root",
+                                          :database => "bikespot"
+                                          )
+end
+
 
 
