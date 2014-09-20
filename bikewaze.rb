27c604e8acec8833e4b6e90bfe72d86781336bc1
Gazal Garg <ggarg@kiwiup.com>
2014-09-20 22:31:08 +0530

    added all api calls.

--- bikewaze.rb
+++ bikewaze.rb
@@ -6,6 +6,10 @@ class Bikepark < ActiveRecord::Base
   self.table_name = "bikeparks"
 end
 
+class Bikeparkstatus < ActiveRecord::Base
+  self.table_name = "bikeparkstatus"
+end
+
 get '/' do
   ActiveRecord::Base.establish_connection(
                                           :adapter  => "mysql2",
@@ -20,42 +24,41 @@ get '/' do
   body 'hello'
 end
 
-get '/checkin' do
-  ActiveRecord::Base.establish_connection(
-                                          :adapter  => "mysql2",
-                                          :host     => "localhost",
-                                          :username => "root",
-                                          :password => "root",
-                                          :database => "bikespot"
-                                          )
+get '/newbikepark' do
+  con
+  @x = params[:x]
+  @y = params[:y]
+  @capacity = params[:capacity]
+  a = Bikepark.new(:x => @x, :y => @y, :capacity => @capacity)
+  a.save
+  body "saved!"
+end
 
+get '/checkin' do
+  con
+  @user = params[:user]
   @x = params[:x]
   @y = params[:y]
   @available = params[:available]
-  a = Bikepark.new(:x => @x, :y => @y, :capacity => @available)
+  @bike_park_id = 1 #Bikepark.find() // Algo Load (@x, @y)
+  a = Bikeparkstatus.new(:user_id => @user, :bike_park_id => @bike_park_id, :available => @available, :time => Time.now)
   a.save
-  # if x.nil?
-  #   body "no"
-  # else
-  #   body  "moo"
-  # end
+  body "Checked IN!!"
 end
 
 get '/availability' do
   con
-  x = Bikepark.last
-  body x.id.to_s
+  @x = params[:x]
+  @y = params[:y]
+  # Write a find query
+  body "Return Number or Y/N"
 end
 
 def con
   ActiveRecord::Base.establish_connection(
-                                          :adapter  => "mysql2",
-                                          :host     => "localhost",
-                                          :username => "root",
-                                          :password => "root",
-                                          :database => "bikespot"
-                                          )
+  :adapter  => "mysql2",
+  :host     => "localhost",
+  :username => "root",
+  :password => "root",
+  :database => "bikespot")
 end
-
-
-
